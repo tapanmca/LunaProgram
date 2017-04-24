b76c2aeecbdb3d471761dac5c40bfd3387309a8d
Tapan kumar Rout <tapanmca2011@gmail.com>
2017-04-24 22:53:06 +0530

    asdf

---
+++ src/com/def/demo/ForTest.java
@@ -0,0 +1,37 @@
+package com.def.demo;
+
+public class ForTest {
+
+	public static void main(String[] args) {
+		char[] ASD={'D','A','D'};
+		// TODO Auto-generated method stub
+		for(int i=0;i<4;i++ )
+		{
+			if(i==3)
+			{
+				System.out.println("MOMMY");
+			}
+			else
+			{
+				for(int j=0;j<=4; j++)
+				{
+					if(j==4)
+					{
+						System.out.println(ASD[i]);
+					}
+					else
+					{
+						System.out.print(" ");
+					}
+					
+				}
+				
+			}
+			
+			
+		}
+		
+
+	}
+
+}
